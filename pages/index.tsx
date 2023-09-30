8e7d3e2fba647eff3a113046dd234a435d7a0e31
0xdorifto <gastaosousafaria@tecnico.ulisboa.pt>
2023-09-30 23:30:15 +0100

    Add very basic structure, abis and router

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,47 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <ConnectButton />
+
+        <div className={styles.modal}>
+          <div>
+            <label for="from">from: </label>
+            <select name="from">
+              <option value="ethereum">Ethereum</option>
+              <option value="polygon">Polygon</option>
+              <option value="arbitrum">Arbitrum</option>
+              <option value="optimism">Optimism</option>
+              <option value="avalanche">Avalanche</option>
+              <option value="base">Base</option>
+              <option value="bnb">BNB</option>
+            </select>
+          </div>
+          <div>
+            <label for="to">to: </label>
+            <select name="to">
+              <option value="ethereum">Ethereum</option>
+              <option value="polygon">Polygon</option>
+              <option value="arbitrum">Arbitrum</option>
+              <option value="optimism">Optimism</option>
+              <option value="avalanche">Avalanche</option>
+              <option value="base">Base</option>
+              <option value="bnb">BNB</option>
+            </select>
+          </div>
+          <div>
+            <label for="token">token: </label>
+            <select name="token">
+              <option value="link">LINK</option>
+              <option value="uni">UNI</option>
+              <option value="mkr">MKR</option>
+              <option value="usdc">USDC</option>
+              <option value="aave">AAVE</option>
+            </select>
+          </div>
+          <div>
+            <label for="amount">amount:</label>
+            <input type="text" id="amount" name="amount"/>
+          </div>
+        </div>
       </main>
     </div>
   );
